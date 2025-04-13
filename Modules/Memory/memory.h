a4abb97c146051e3d2cbb38af438ca885b7449ea
canulasu <jakarandainnovation@gmail.com>
2025-04-13 21:26:49 +0300

    fix semicolon issue

--- Modules/Memory/memory.h
+++ Modules/Memory/memory.h
@@ -3,10 +3,10 @@
 
 #include <Arduino.h>
 
-extern String stringslot1 = ""
-extern String stringslot2 = ""
-extern String stringslot3 = ""
-extern String stringslot4 = ""
-extern String stringslot5 = ""
+extern String stringslot1 = "";
+extern String stringslot2 = "";
+extern String stringslot3 = "";
+extern String stringslot4 = "";
+extern String stringslot5 = "";
 
 #endif
