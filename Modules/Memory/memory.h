919767c3021026f345d18241a22e784423ce39b5
canulasu <jakarandainnovation@gmail.com>
2025-04-13 21:23:40 +0300

    still working on globalization

--- Modules/Memory/memory.h
+++ Modules/Memory/memory.h
@@ -1,5 +1,12 @@
+#ifndef MEMORY_H
+#define MEMORY_H
+
+#include <Arduino.h>
+
 extern String stringslot1 = ""
 extern String stringslot2 = ""
 extern String stringslot3 = ""
 extern String stringslot4 = ""
 extern String stringslot5 = ""
+
+#endif
