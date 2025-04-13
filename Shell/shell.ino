f03dfff611327373e760c4d00b8004c755645ec5
canulasu <jakarandainnovation@gmail.com>
2025-04-13 22:05:36 +0300

    Update shell.ino

--- Shell/shell.ino
+++ Shell/shell.ino
@@ -1,8 +1,17 @@
-void shell() {
+void print_info() {
+  Serial.println("Kedi Kernel running on Arduino C");
+  Serial.println("Version 0.0.1");
+}
 
+void start_serial() {  
   Serial.begin(9600);
   delay(3000);
   Serial.print("Serial communication started on 9600 by shell.");
+}
+
+void shell() {
+
+  start_serial();
   
   Serial.print("pati>>> ");
   while (Serial.available() == 0) {
@@ -12,8 +21,7 @@ void shell() {
 
   Serial.println(command);
 
-  if (command == "exit") {
-    Serial.println("Kernel will stop serial communications with user");
-    Serial.end();
+  if (command == "info") {
+    print_info();
   }
 }
