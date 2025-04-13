c82c41c76420b36bbe970a0335007754d3b81613
canulasu <jakarandainnovation@gmail.com>
2025-04-13 21:36:34 +0300

    fix extern var issue and add definitions for int slot for RAM drivers

--- Modules/Memory/memory.h
+++ Modules/Memory/memory.h
@@ -3,10 +3,16 @@
 
 #include <Arduino.h>
 
-extern String stringslot1 = "";
-extern String stringslot2 = "";
-extern String stringslot3 = "";
-extern String stringslot4 = "";
-extern String stringslot5 = "";
+extern String stringslot1;
+extern String stringslot2;
+extern String stringslot3;
+extern String stringslot4;
+extern String stringslot5;
+
+extern int intslot1;
+extern int intslot2;
+extern int intslot3;
+extern int intslot4;
+extern int intslot5;
 
 #endif
