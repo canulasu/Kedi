5b56287f5d1081be8252164926e685bf5ce819b4
canulasu <jakarandainnovation@gmail.com>
2025-04-13 21:37:40 +0300

    Update memory.cpp

--- Modules/Memory/memory.cpp
+++ Modules/Memory/memory.cpp
@@ -5,3 +5,9 @@ String stringslot2 = "";
 String stringslot3 = "";
 String stringslot4 = "";
 String stringslot5 = "";
+
+int intslot1 = 1;
+int intslot2 = 1;
+int intslot3 = 1;
+int intslot4 = 1;
+int intslot5 = 1;
