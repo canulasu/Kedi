7868c77168b3da8f763310d4713c8ee591433b91
canulasu <jakarandainnovation@gmail.com>
2025-04-14 20:47:47 +0300

    modularize startup and remove cpu cooldown

--- Bootloader/boot.ino
+++ Bootloader/boot.ino
@@ -1,12 +1,14 @@
-void setup() {
+void start_serial() {
     Serial.begin(9600);
     delay(3000);
     Serial.println("Serial Communication Initiated on 9600.");
+}
+
+void setup() {
+    Serial.println("Serial Communication Initiated on 9600.");
     Serial.println("Kedi Bootloader Version 0.0.1");
     Serial.println("---------------------------------");
-    Serial.println("Bootloader will now start CPU cooldown.");
-    delay(10000);
-    Serial.println("Bootloader will now boot Kedi userspace init program.");
+    Serial.println("Bootloader will now boot Kedi userspace api program.");
 }
 
 void loop() {
